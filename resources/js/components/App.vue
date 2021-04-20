6b7a87eab90a4abce371973cde40265e351f4629
noe martinez <mr18116@ues.edu.sv>
2021-04-20 16:52:35 -0600

    configuracion general del proyecto

---
+++ resources/js/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app>
+        <v-app-bar app flat>
+            app-bar
+        </v-app-bar>
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+        <v-footer>
+            footer
+        </v-footer>
+    </v-app>
+    
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
